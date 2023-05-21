a2c71760c035fb1c004a44c9d050262f1c1660e1
emilynievesb <nemily1710@gmail.com>
2023-05-21 18:43:23 -0500

    feat: :zap: Add category filtering function to fetch.js file
    This commit adds a new exportable function to the `fetch.js` file that allows filtering elements based on their category. This function enhances the functionality of the code by providing a convenient way to perform category-based filters on the elements obtained from the API.

--- js/fetch.js
+++ js/fetch.js
@@ -6,7 +6,13 @@ export async function getCategories(URL) {
 }
 export async function getAllProducts(URL) {
   const response = await fetch(`${URL}products`);
-  const categories = await response.json();
-  console.log(categories);
-  return categories;
+  const products = await response.json();
+  console.log(products);
+  return products;
+}
+export async function getCategoryProducts(URL, category) {
+  const response = await fetch(`${URL}products/category/${category}`);
+  const products = await response.json();
+  console.log(products);
+  return products;
 }
