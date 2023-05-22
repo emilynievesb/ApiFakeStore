4de7b2da5614ad6867af8fc9b6c5d0d724147fd5
emilynievesb <nemily1710@gmail.com>
2023-05-21 19:32:45 -0500

    feat: :zap: Add HTML and JS files for graphical interfaces of data filtering from the API
    This commit adds HTML files and JS files that are part of the graphical interfaces for filtering data from the API. The HTML files provides the structure and necessary elements to display and filter the data obtained from the API. The JS files contains the logic to perform filtering operations and update the user interface accordingly.

---
+++ js/men.js
@@ -0,0 +1,36 @@
+import { createCards } from "./cards.js";
+import { getCategoryProducts, getCategories } from "./fetch.js";
+
+const URL = "https://fakestoreapi.com/";
+
+async function dinamicCategories() {
+  const categories = await getCategories(URL);
+  for (let i = 0; i < categories.length; i++) {
+    let node = document.createElement("li");
+    let a = document.createElement("a");
+    switch (i) {
+      case 0:
+        a.href = "../html/electronics.html";
+        break;
+      case 1:
+        a.href = "../html/jewelery.html";
+        break;
+      case 2:
+        a.href = "../html/men.html";
+        break;
+      case 3:
+        a.href = "../html/women.html";
+        break;
+    }
+    let textnode = document.createTextNode(categories[i]);
+    a.appendChild(textnode);
+    node.append(a);
+    document.getElementById("categories").appendChild(node);
+  }
+}
+async function dinamicProducts() {
+  const products = await getCategoryProducts(URL, "men's clothing");
+  createCards(products);
+}
+dinamicCategories();
+dinamicProducts();
